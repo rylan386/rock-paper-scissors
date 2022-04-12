294ca4b410014e990cec60054e24ed2647b87288
Rylan <ornelasrylan@rocketmail.com>
2022-04-12 10:11:53 -0700

    Finished the working game

--- script.js
+++ script.js
@@ -1,66 +1,89 @@
 let playerScore = 0;
 let computerScore = 0;
-let computerSelection = computerPlay();
+const humanScore = document.getElementById("box1");
+const comScore = document.getElementById("box2");
+const rock2 = document.getElementById("rock2");
+const paper2 = document.getElementById("paper2");
+const scissors2 = document.getElementById("scissors2");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const results = document.getElementById("outcome");
+const reset = document.getElementById("reset")
+let computerSelection;
+let playerSelection;
 
-//Converts player input to lower case
-function lowerCase(str) {
-    const lowerCaseStr = str.toLowerCase();
-    return lowerCaseStr;
-}
+rock.addEventListener("click", () => {
+    computerSelection = computerPlay();
+    playerSelection = "rock";
+    playRound(playerSelection, computerSelection);
+});
+paper.addEventListener("click", () => {
+    computerSelection = computerPlay();
+    playerSelection = "paper";
+    playRound(playerSelection, computerSelection);
+});
+scissors.addEventListener("click", () => {
+    computerSelection = computerPlay();
+    playerSelection = "scissors";
+    playRound(playerSelection, computerSelection);
+});
+reset.addEventListener("click", () => {
+    location.reload()
+})
 
 //Computer randomly generates "rock", "paper", or "scissors"
 function computerPlay() {
-    let game = ['rock', 'paper', 'scissors'];
-    let words = game[Math.floor(Math.random() * game.length)];
-    return words;
+    let words = ["rock", "paper", "scissors"];
+    return words[Math.floor(Math.random() * words.length)]
 }
 
 //Plays a single round and tests wheather the computer or player won or lost
 function playRound(playerSelection, computerSelection) {
-    playerSelection = lowerCase(playerSelection);
-
-    if (playerSelection == computerSelection) {
-        return `You selected ${playerSelection} \nComputer chose ${computerSelection}`;
-    }
-    else if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-            (playerSelection == 'paper' && computerSelection == 'rock') ||
-            (playerSelection == 'scissors' && computerSelection == 'paper')) {
+    if (playerScore < 5 && computerScore < 5) {
+    if ((playerSelection === "rock" && computerSelection === "rock") ||
+        (playerSelection === "paper" && computerSelection === "paper") ||
+        (playerSelection === "scissors" && computerSelection === "scissors")) {
+            results.textContent = "Draw";
+        }
+    else if ((playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "paper" && computerSelection === "rock") ||
+            (playerSelection === "scissors" && computerSelection === "paper")) {
                 playerScore++;
-                return `You selected ${playerSelection} \nComputer chose ${computerSelection}`;
+                humanScore.textContent = playerScore;
+                results.textContent = "You won this round!";
             }
-    else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
-            (playerSelection == 'paper' && computerSelection == 'scissors') ||
-            (playerSelection == 'scissors' && computerSelection == 'rock')) {
+    else if ((playerSelection === "rock" && computerSelection === "paper") ||
+            (playerSelection === "paper" && computerSelection === "scissors") ||
+            (playerSelection === "scissors" && computerSelection === "rock")) {
                 computerScore++;
-                return `You selected ${playerSelection} \nComputer chose ${computerSelection}`;
+                comScore.textContent = computerScore;
+                results.textContent = "You lost this round:(";
             }
-    else {
-        return 'Please enter a valid choice.';
-    }
+   if (computerSelection === "rock") {
+    rock2.style.borderColor = "black";
+    paper2.style.borderColor = null;
+    scissors2.style.borderColor = null;
+   }
+   else if (computerSelection === "paper") {
+    paper2.style.borderColor = "black";
+    rock2.style.borderColor = null;
+    scissors2.style.borderColor = null;
+}
+    else if (computerSelection === "scissors") {
+    scissors2.style.borderColor = "black";
+    paper2.style.borderColor = null;
+    rock2.style.borderColor = null;
+}
 }
 
-//Displays win or lose status for the current round
-function displayWinner() {
-    if (playerScore == computerScore) {
-        console.log('Draw');
-    }
-    else if (playerScore > computerScore) {
-        console.log('You won!!');
-    }
-    else 
-        console.log('sorry you lost:(');
+     if (playerScore == 5) {
+        results.textContent = "You win the game!!"
+        reset.style.visibility = "visible";
+        }
+    else if (computerScore == 5) {
+        results.textContent = "You lost the game:("
+        reset.style.visibility = "visible";
 }
-
-//creates a loops to play 5 rounds and ask the player to chose an option
-function game () {
-
-    for (i=0; i<5; i++){
-        playerSelection = prompt('rock, paper, or scissors?');
-        computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Player score: ${playerScore} | Computer score ${computerScore}`);
-     }
-    displayWinner();
 }
-
-game();
+playRound()
